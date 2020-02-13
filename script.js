8fd6d1af9e924ff1c848bdb3ce430224ec0ce6cf
MFvanRossum <matissev@hotmail.com>
2020-02-13 15:01:04 -0500

    Bodies clear when they move too far outside the canvas

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ class Game {
         this.bodies = this.bodies.concat(new Player(this, gameSize))
         let tick = () => {
             this.update()
-            this.draw(screen, gameSize) 
+            this.draw(screen, gameSize)
             requestAnimationFrame(tick)
         }
         tick()
@@ -17,10 +17,15 @@ class Game {
         let notColliding = (b1) => {
             return this.bodies.filter(function (b2) { return colliding(b1, b2) }).length === 0
         }
+        let canvas = document.getElementById('pixel-war')
         this.bodies = this.bodies.filter(notColliding)
+        this.bodies = this.bodies.filter(body => body.center.y >= 0 - 50)
+        this.bodies = this.bodies.filter(body => body.center.y <= canvas.height + 50)
         for (let i = 0; i < this.bodies.length; i++) {
             this.bodies[i].update()
         }
+        // let intervalInMilliseconds = 5000
+        // setInterval(createEnemies, intervalInMilliseconds)
     }
 
     draw(screen, gameSize) {
@@ -30,7 +35,7 @@ class Game {
         }
     }
 
-    addBody (body) {
+    addBody(body) {
         let isColliding = this.bodies.some(otherBody => colliding(body, otherBody) && body.prototype === otherBody.prototype)
         if (!isColliding) {
             this.bodies.push(body)
@@ -39,12 +44,12 @@ class Game {
 }
 
 class Enemy {
-    constructor (game, center) {
-    this.game = game
-    this.center = center
-    this.size = { x: 15, y: 20 }
-    this.moveY = 0
-    this.speedY = 1
+    constructor(game, center) {
+        this.game = game
+        this.center = center
+        this.size = { x: 15, y: 20 }
+        this.moveY = 0
+        this.speedY = 1
     }
     update() {
         this.center.y += this.speedY
@@ -57,14 +62,15 @@ function createEnemies(game) {
     for (let i = 0; i < 10; i++) {
         let x = Math.random() * 300
         let y = -30
-        enemies.push(new Enemy(game, { x: x, y: y}))
+        let intervalInMilliseconds = 500
+        setInterval(enemies.push(new Enemy(game, { x: x, y: y })), intervalInMilliseconds)
     }
     return enemies
 }
 
 class Player {
-    constructor (game, gameSize) {
-        this.game = game 
+    constructor(game, gameSize) {
+        this.game = game
         this.size = { x: 15, y: 15 }
         this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.y * 2 }
 
@@ -74,12 +80,12 @@ class Player {
     update() {
         if (this.center.x > 0) {
             if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
-            this.center.x -= 2
+                this.center.x -= 2
             }
-        } 
+        }
         if (this.center.x < 300) {
             if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
-            this.center.x += 2
+                this.center.x += 2
             }
         }
 
@@ -87,14 +93,14 @@ class Player {
             let bullet = new Bullet({ x: this.center.x, y: this.center.y - this.size.y - 10 },
                 { x: 0, y: -7 })
             this.game.addBody(bullet)
-            
-            }           
+
         }
     }
+}
 
 
 class Bullet {
-    constructor (center, velocity) {
+    constructor(center, velocity) {
         this.center = center
         this.size = { x: 5, y: 8 }
         this.velocity = velocity
@@ -110,18 +116,18 @@ class Bullet {
     }
 }
 
-function drawRect (screen, body) {
+function drawRect(screen, body) {
     screen.fillRect(body.center.x - body.size.x / 2, body.center.y - body.size.y / 2,
         body.size.x, body.size.y)
 }
 
-function colliding (b1, b2) {
+function colliding(b1, b2) {
     return !(
         b1 === b2 ||
-            b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x / 2 ||
-            b1.center.y + b1.size.y / 2 < b2.center.y - b2.size.y / 2 ||
-            b1.center.x - b1.size.x / 2 > b2.center.x + b2.size.x / 2 ||
-            b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y / 2
+        b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x / 2 ||
+        b1.center.y + b1.size.y / 2 < b2.center.y - b2.size.y / 2 ||
+        b1.center.x - b1.size.x / 2 > b2.center.x + b2.size.x / 2 ||
+        b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y / 2
     )
 }
 
